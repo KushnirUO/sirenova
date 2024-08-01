4c963d831ea4bf4cec1a77b95d27600163f10df9
KushnirUO <62049925+KushnirUO@users.noreply.github.com>
2024-08-01 21:24:58 +0300

    fix

--- wp-content/themes/sirenova/front-page.php
+++ wp-content/themes/sirenova/front-page.php
@@ -2,12 +2,70 @@
         get_header();
         ?>
 
-        dsdssdd
-        dsdssdd
-        <p>ffdfd</p>
-        <p>ffdfd</p>
-        <p>ffdfd</p>
-        <?php
+        <section class="promo-section">
+                <img class="promo-bg" src="https://sirenova.com.ua/wp-content/themes/v2.3/img/bg-first-sect.png" alt="">
 
+                <div class="promo-section__wrapper">
+                        <div class="promo-text">
+
+                                <h1>Білизна, яка варта твоєї уваги</h1>
+                                <div class="gift-promo">
+                                        <img class="gift-promo-img" src="https://sirenova.com.ua/wp-content/themes/v2.3/img/gift-promo.png" alt="">
+                                        <p>Замовляй комплект - отримуй трусики у подарунок</p>
+                                </div>
+                                <a href="https://sirenova.com.ua/shop/" target="" class="btn">Перейти до магазину</a>
+                        </div>
+                        <img class="promo-image" src="https://sirenova.com.ua/wp-content/themes/v2.3/img/img-first-sect.png" alt="">
+                </div>
+
+        </section>
+
+
+
+        <section class="wrapper main__categoryes">
+                <h2>Категорії</h2>
+                <div class="main__categoryes-blocks">
+
+                        <a class="block" href="https://sirenova.com.ua/komplekty-belya/">
+                                <div class="block__bg"><img src="https://sirenova.com.ua/wp-content/uploads/2024/03/IMG_7921.jpg" alt=""></div>
+                                <div class="block__content">
+                                        <h3>Комплекти білизни</h3>
+                                </div>
+                        </a>
+                        <a class="block" href="https://sirenova.com.ua/komplekty-belya/">
+                                <div class="block__bg"><img src="https://sirenova.com.ua/wp-content/uploads/2024/03/IMG_7921.jpg" alt=""></div>
+                                <div class="block__content">
+                                        <h3>Комплекти білизни</h3>
+                                </div>
+                        </a>
+                        <a class="block" href="https://sirenova.com.ua/komplekty-belya/">
+                                <div class="block__bg"><img src="https://sirenova.com.ua/wp-content/uploads/2024/03/IMG_7921.jpg" alt=""></div>
+                                <div class="block__content">
+                                        <h3>Комплекти білизни</h3>
+                                </div>
+                        </a>
+                        <a class="block" href="https://sirenova.com.ua/komplekty-belya/">
+                                <div class="block__bg"><img src="https://sirenova.com.ua/wp-content/uploads/2024/03/IMG_7921.jpg" alt=""></div>
+                                <div class="block__content">
+                                        <h3>Комплекти білизни</h3>
+                                </div>
+                        </a>
+                        <a class="block" href="https://sirenova.com.ua/komplekty-belya/">
+                                <div class="block__bg"><img src="https://sirenova.com.ua/wp-content/uploads/2024/03/IMG_7921.jpg" alt=""></div>
+                                <div class="block__content">
+                                        <h3>Комплекти білизни</h3>
+                                </div>
+                        </a>
+                        <a class="block" href="https://sirenova.com.ua/komplekty-belya/">
+                                <div class="block__bg"><img src="https://sirenova.com.ua/wp-content/uploads/2024/03/IMG_7921.jpg" alt=""></div>
+                                <div class="block__content">
+                                        <h3>Комплекти білизни</h3>
+                                </div>
+                        </a>
+
+                </div>
+        </section>
+
+        <?php
         get_footer();
         ?>
