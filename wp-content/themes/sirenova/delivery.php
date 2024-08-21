2750b603cf243c175831a8471ee52b49267f98c8
immgoo <56151630+immgoo@users.noreply.github.com>
2024-08-21 16:32:31 +0300

    policy about delivery back

--- wp-content/themes/sirenova/delivery.php
+++ wp-content/themes/sirenova/delivery.php
@@ -5,8 +5,9 @@
  */
 get_header()
 
-?>
+    ?>
 <section class="wrapper post delivery_and-payment">
+    <?php woocommerce_breadcrumb(); ?>
     <div class="post__content">
         <h1><?php the_title(); ?></h1>
         <?php the_content(); ?>
--- wp-content/themes/sirenova/privacy-policy.php
+++ wp-content/themes/sirenova/privacy-policy.php
@@ -1,11 +1,15 @@
 <?
+/**
+ * Template Name: Privacy-policy
+ */
 get_header();
 ?>
 <section class="wrapper post">
+    <?php woocommerce_breadcrumb(); ?>
     <div class="post__content">
         <h1><?php the_title(); ?></h1>
         <?php the_content(); ?>
     </div>
 </section>
 
-<? get_footer();
+<?php get_footer();
