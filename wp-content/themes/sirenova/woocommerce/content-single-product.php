b6fd0ef6db869891003891a9b35c8498d341e37a
KushnirUO <62049925+KushnirUO@users.noreply.github.com>
2024-08-30 15:55:40 +0300

    fix

--- wp-content/themes/sirenova/woocommerce/content-single-product.php
+++ wp-content/themes/sirenova/woocommerce/content-single-product.php
@@ -70,7 +70,7 @@ echo implode(' ', $classes);
     // Якщо продукт варіативний
     if ($product->is_type('variable')) {
         $variation_ids = $product->get_children(); // Отримуємо ID всіх варіацій
-    
+
         foreach ($variation_ids as $variation_id) {
             $variation = wc_get_product($variation_id);
 
@@ -132,123 +132,6 @@ echo implode(' ', $classes);
     </div>
     <div class="info">
         <h2><?php the_title(); ?></h2>
-<<<<<<< HEAD
-        <?php wc_get_template_part('loop/price-single'); ?>
-        <div class="cart__counter">
-            <span>К-сть:</span>
-            <input type="number" name="quantity" value="1">
-            <span class="increase"></span>
-            <span class="decrease"></span>
-        </div>
-        <div class="info__colors">
-            <?php
-            if ($product->is_type('variable')) {
-                $available_variations = $product->get_available_variations();
-                $attributes = $product->get_variation_attributes();
-
-                // Масив для збереження залишків
-                $stock_data = [];
-
-                if (isset($attributes['pa_color'])) {
-                    $colors = $attributes['pa_color'];
-
-                    if (!empty($colors)) {
-                        echo '<span>КОЛІР:</span>';
-                        echo '<div class="pallete">';
-                        foreach ($colors as $color_slug) {
-                            $term = get_term_by('slug', $color_slug, 'pa_color');
-                            $color_hex = get_term_meta($term->term_id, 'attribute_color', true);
-
-                            foreach ($available_variations as $variation) {
-                                // Перевірка наявності обох атрибутів
-                                if (isset($variation['attributes']['attribute_pa_color']) && $variation['attributes']['attribute_pa_color'] == $color_slug) {
-                                    $variation_obj = new WC_Product_Variation($variation['variation_id']);
-                                    $stock_quantity = $variation_obj->get_stock_quantity();
-                                    $variation_id = $variation['variation_id'];
-
-                                    // Перевірка чи варіація зберігається вже в масиві $stock_data
-                                    if (!isset($stock_data[$variation_id])) {
-                                        $stock_data[$variation_id] = [
-                                            'color' => $color_slug,
-                                            'size' => isset($variation['attributes']['attribute_pa_size']) ? $variation['attributes']['attribute_pa_size'] : '',
-                                            'stock_quantity' => $stock_quantity
-                                        ];
-                                    }
-                                }
-                            }
-
-                            echo '<div class="pallete-one">';
-                            echo '<input type="radio" name="color" id="color-' . esc_attr($term->slug) . '" value="' . esc_attr($term->slug) . '" style="background-color: ' . esc_attr($color_hex) . ';" />';
-                            echo '<label for="color-' . esc_attr($term->slug) . '" style="background-color: ' . esc_attr($color_hex) . '"></label>';
-                            echo $term->name;
-                            echo '</div>';
-                        }
-                        echo '</div>';
-                    }
-                }
-            }
-            ?>
-        </div>
-
-        <div class="size__dropdown">
-            <?php
-            if ($product->is_type('variable')) {
-                $attributes = $product->get_variation_attributes();
-
-                if (isset($attributes['pa_size'])) {
-                    $sizes = $attributes['pa_size'];
-
-                    if (!empty($sizes)) {
-                        echo '<span>РОЗМІР:</span>';
-                        echo '<div class="size__dropdown-content">';
-                        foreach ($sizes as $size_slug) {
-                            $term = get_term_by('slug', $size_slug, 'pa_size');
-                            if ($term) {
-                                foreach ($available_variations as $variation) {
-                                    // Перевірка наявності обох атрибутів
-                                    if (isset($variation['attributes']['attribute_pa_size']) && $variation['attributes']['attribute_pa_size'] == $size_slug) {
-                                        $variation_obj = new WC_Product_Variation($variation['variation_id']);
-                                        $stock_quantity = $variation_obj->get_stock_quantity();
-                                        $variation_id = $variation['variation_id'];
-
-                                        // Перевірка чи варіація зберігається вже в масиві $stock_data
-                                        if (!isset($stock_data[$variation_id])) {
-                                            $stock_data[$variation_id] = [
-                                                'color' => isset($variation['attributes']['attribute_pa_color']) ? $variation['attributes']['attribute_pa_color'] : '',
-                                                'size' => $size_slug,
-                                                'stock_quantity' => $stock_quantity
-                                            ];
-                                        }
-                                    }
-                                }
-
-                                echo "<div class='sizes-single'>";
-                                echo '<input type="radio" name="size" id="size-' . esc_attr($term->slug) . '" value="' . esc_attr($term->slug) . '" />';
-                                echo '<label for="size-' . esc_attr($term->slug) . '">' . esc_html($term->name) . '</label>';
-                                echo '</div>';
-                            }
-                        }
-                        echo '</div>';
-                    }
-                }
-            }
-            ?>
-        </div>
-
-        <script>
-            var stockData = <?php echo json_encode($stock_data); ?>;
-            console.log(stockData);
-        </script>
-
-
-        <div class="info__btns">
-            <div class="info__btns-like">
-                <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M11.9369 1.59554C11.2645 0.872795 10.3222 0.461561 9.33508 0.460037C8.34712 0.46118 7.40392 0.872196 6.73047 1.59508L6.50095 1.83761L6.27143 1.59508C4.93513 0.156852 2.68589 0.0742567 1.24769 1.41056C1.18396 1.4698 1.12241 1.53132 1.06317 1.59508C-0.354391 3.1241 -0.354391 5.48713 1.06317 7.01614L6.16366 12.3949C6.34012 12.5812 6.63418 12.5892 6.82047 12.4127C6.82657 12.407 6.8325 12.401 6.83827 12.3949L11.9369 7.01614C13.3544 5.48729 13.3544 3.1244 11.9369 1.59554ZM11.2646 6.37637H11.2641L6.50095 11.4002L1.73733 6.37637C0.654391 5.20806 0.654391 3.4027 1.73733 2.23439C2.72077 1.16849 4.38212 1.10166 5.44801 2.0851C5.49977 2.13285 5.54956 2.18264 5.59731 2.23439L6.16366 2.83188C6.3503 3.01733 6.65163 3.01733 6.83827 2.83188L7.40463 2.23485C8.38806 1.16896 10.0494 1.10212 11.1153 2.08556C11.1671 2.13331 11.2169 2.1831 11.2646 2.23485C12.357 3.40501 12.365 5.21367 11.2646 6.37637Z"
-                        fill="black"></path>
-                </svg>
-=======
         <?php
         if ($has_stock):
             wc_get_template_part('loop/price-single'); ?>
@@ -306,7 +189,6 @@ echo implode(' ', $classes);
                     }
                 }
                 ?>
->>>>>>> 040e5f6dc64a6421fe229b21c37f7ae70b6e932a
             </div>
 
             <div class="size__dropdown">
