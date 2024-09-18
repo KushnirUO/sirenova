6b4ca59eff4fe8f55add4f9ab0d1cf4325bd39ca
KushnirUO <62049925+KushnirUO@users.noreply.github.com>
2024-09-18 15:21:23 +0300

    fix

--- wp-content/themes/sirenova/functions.php
+++ wp-content/themes/sirenova/functions.php
@@ -11,8 +11,7 @@ add_action('after_setup_theme', function () {
 // Add ACF Option page
 if (function_exists('acf_add_options_page')) {
     acf_add_options_page();
-}
-;
+};
 
 function set_sale_page_flag()
 {
@@ -134,13 +133,13 @@ add_action('woocommerce_thankyou', 'remove_shipping_address_column', 1);
 
 function remove_shipping_address_column()
 {
-    ?>
+?>
     <style>
         .woocommerce-column.woocommerce-column--2.woocommerce-column--shipping-address.col-2 {
             display: none !important;
         }
     </style>
-    <?php
+<?php
 }
 
 // Переміщення товарів без наявності в кінець списку і сортування по новизні
@@ -179,4 +178,137 @@ add_action('pre_get_posts', 'custom_sort_out_of_stock_to_end_and_by_date');
 function is_sale_page()
 {
     return is_page() && get_the_ID() === wc_get_page_id('sale');
+}
+
+
+
+// Додаємо хук на сторінку "Дякую"
+add_action('woocommerce_thankyou', 'redirect_to_wayforpay_if_cod', 10, 1);
+
+function redirect_to_wayforpay_if_cod($order_id)
+{
+    $order = wc_get_order($order_id);
+
+    // Перевіряємо, чи метод оплати "Готівка при отриманні"
+    if ('cod' === $order->get_payment_method()) {
+        // Фіксована сума для WayForPay
+        $amount = 10;
+
+        // Генеруємо посилання для редиректу на WayForPay
+        $wayforpay_form = generate_wayforpay_payment_form($order_id, $amount);
+
+        // Виводимо форму для оплати та автоматично відправляємо її
+        echo $wayforpay_form;
+
+        // Зупиняємо подальше виконання коду, щоб уникнути редиректу на стандартну сторінку "Дякую"
+        exit;
+    }
+}
+
+function generate_signature($merchantAccount, $orderReference, $orderDate, $amount, $currency, $orderTimeout, $productName, $productPrice, $productCount, $clientFirstName, $clientLastName, $clientAddress, $clientCity, $clientEmail, $clientPhone, $clientCountry, $clientRegion, $merchantDomainName, $defaultPaymentSystem, $secretKey)
+{
+    $signatureString = implode(';', array(
+        $merchantAccount,
+        $merchantDomainName,
+        $orderReference,
+        $orderDate,
+        $amount,
+        $currency,
+        $productName,
+        $productCount,
+        $productPrice,
+    ));
+    // return $signatureString;
+
+    return hash_hmac('md5', $signatureString, $secretKey);
+}
+function generate_wayforpay_payment_form($order_id, $amount)
+{
+    $order = wc_get_order($order_id);
+
+    // Основні параметри WayForPay
+    $merchantAccount = 'test_merch_n1';
+    $secretKey = 'flk3409refn54t54t*FNJRET';
+    $currency = 'UAH';
+    $productName = 'Фіксована оплата';
+    $productCount = 1;
+    $productPrice = $amount;
+    $orderDate = time();
+    $clientEmail = $order->get_billing_email();
+    $clientFirstName = $order->get_billing_first_name();
+    $clientLastName = $order->get_billing_last_name();
+    $clientAddress = $order->get_billing_address_1();
+    $clientCity = $order->get_billing_city();
+    $clientPhone = $order->get_billing_phone(); // Якщо потрібно
+    $clientCountry = ''; // Якщо потрібно
+    $clientRegion = ''; // Якщо потрібно
+    $merchantDomainName =
+        parse_url(home_url(), PHP_URL_HOST);
+    $orderTimeout = '49000';
+    $defaultPaymentSystem = 'card';
+    $merchantTransactionSecureType = 'AUTO'; // Доданий параметр безпеки
+    // $returnUrl = $thank_you_page_url;
+
+
+    // Генерація підпису для WayForPay
+    $signature = generate_signature(
+        $merchantAccount,
+        $order_id,
+        $orderDate,
+        $amount,
+        $currency,
+        $orderTimeout,
+        $productName,
+        $productPrice,
+        $productCount,
+        $clientFirstName,
+        $clientLastName,
+        $clientAddress,
+        $clientCity,
+        $clientEmail,
+        $clientPhone,
+        $clientCountry,
+        $clientRegion,
+        $merchantDomainName,
+        $defaultPaymentSystem,
+        $secretKey
+    );
+
+    // URL для повернення на сайт після успішної оплати
+    $returnUrl = add_query_arg('key', $order->get_order_key(), $order->get_checkout_order_received_url());
+    // Генерація HTML форми для WayForPay
+    $form = '<form method="post" action="https://secure.wayforpay.com/pay" id="wayforpay_payment_form" accept-charset="utf-8" style="display:none">';
+    $form .= '<input name="merchantAccount" value="' . esc_attr($merchantAccount) . '">';
+    $form .= '<input name="merchantAuthType" value="SimpleSignature">';
+    $form .= '<input name="merchantDomainName" value="' . esc_attr($merchantDomainName) . '">';
+    $form .= '<input name="orderReference" value="' . esc_attr($order_id) . '">';
+    $form .= '<input name="orderDate" value="' . esc_attr($orderDate) . '">';
+    $form .= '<input name="amount" value="' . esc_attr($amount) . '">';
+    $form .= '<input name="currency" value="' . esc_attr($currency) . '">';
+    $form .= '<input name="orderTimeout" value="' . esc_attr($orderTimeout) . '">';
+
+    $form .= '<input name="productName[]" value="' . esc_attr($productName) . '">';
+    $form .= '<input name="productPrice[]" value="' . esc_attr($productPrice) . '">';
+    $form .= '<input name="productCount[]" value="' . esc_attr($productCount) . '">';
+
+    $form .= '<input name="clientFirstName" value="' . esc_attr($clientFirstName) . '">';
+    $form .= '<input name="clientLastName" value="' . esc_attr($clientLastName) . '">';
+    $form .= '<input name="clientAddress" value="' . esc_attr($clientAddress) . '">';
+    $form .= '<input name="clientCity" value="' . esc_attr($clientCity) . '">';
+    $form .= '<input name="clientEmail" value="' . esc_attr($clientEmail) . '">';
+    $form .= '<input name="clientPhone" value="' . esc_attr($clientPhone) . '">'; // Якщо потрібно
+    $form .= '<input name="clientCountry" value="' . esc_attr($clientCountry) . '">'; // Якщо потрібно
+    $form .= '<input name="clientRegion" value="' . esc_attr($clientRegion) . '">'; // Якщо потрібно
+    $form .= '<input name="defaultPaymentSystem" value="' . esc_attr($defaultPaymentSystem) . '">';
+    $form .= '<input name="merchantSignature" value="' . esc_attr($signature) . '">';
+    $form .= '<input name="merchantTransactionSecureType" value="' . esc_attr($merchantTransactionSecureType) . '">';
+    $form .= '<input name="returnUrl" value="' . esc_attr($returnUrl) . '">';
+
+
+    $form .= '<input type="submit" value="Оплатити">';
+    $form .= '</form>';
+    $form .= '<script type="text/javascript">
+                document.getElementById("wayforpay_payment_form").submit();
+              </script>';
+    return $form;
 }
